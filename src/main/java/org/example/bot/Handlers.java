88e4e26ecaf00f679de934259af07a8382d120fc
sharkioso <tetkin04@mail.ru>
2024-10-05 17:39:36 +0500

    тех долг по первой задаче

--- src/main/java/org/example/bot/Handlers.java
+++ src/main/java/org/example/bot/Handlers.java
@@ -7,43 +7,38 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 import static org.example.bot.FSMHandler.FSMCommands;
 
 
 public class Handlers extends TelegramBot {
-    Map<String, Runnable> commands = new LinkedHashMap<>();
+    Map<String, Consumer<Long>> commands = new LinkedHashMap<>();
+
     Map<String, String> helpText = new LinkedHashMap<>();
-
     public String answer = "";
-    public void telegramHandlers(String messageText, Long chatId, Update update) {
-        commands.put("/start", () -> startCommand(chatId));
+    public Handlers(){
+        commands.put("/start",this::startCommand);
+        commands.put("/authors",this::authorsCommand);
+        commands.put("about",this::aboutCommand);
+
         helpText.put("start", "Это команда для начала нашего общения");
-
-        commands.put("/authors", () -> authorsCommand(chatId));
         helpText.put("authors", "Это команда, которая расскажет тебе, кто написал этого бота");
-
-        commands.put("/about", () -> aboutCommand(chatId));
         helpText.put("about", "Эта команда описывает бота");
-
-        commands.put("/registr",() -> reg(chatId,update));
-        helpText.put("registr", "Это команда для регистрации");
-
         helpText.put("/help", "Бот имеет следующие команды: \n/start\n/about\n/authors\n/help\nДля подробной информации введите \"/help команда\"");
         helpText.put("help", "Вводя эту команду вы можете узнать какие команды умеет делать бот");
-
+    }
+    public void telegramHandlers(Long chatId,String messageText) {
         if (messageText.contains("/help")) {
             if (messageText.length() > 6) {
                 messageText = messageText.substring(6);
             }
-            answer = helpText.get(messageText);
+            answer =helpText.get(messageText);
             sendMessage(chatId, helpText.get(messageText));
-        } else if (FSM.currentState != "not working"){
-            FSMHandler automat = new FSMHandler(chatId, update);
-            FSMCommands.get(FSM.currentState).run();
         } else {
             if (commands.containsKey(messageText)) {
-                commands.get(messageText).run();
+                commands.get(messageText).accept(chatId);
             } else {
                 answer="Нет такой команды\nЯ вас не понимаю";
                 sendMessage(chatId, "Нет такой команды\nЯ вас не понимаю");
